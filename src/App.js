dcd1196d903169a4a88ed495ecfcdedfcafe54b9
michellenikeetha <88478424+michellenikeetha@users.noreply.github.com>
2023-07-13 09:47:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+const App = () =>
+{
+  return(
+    <>
+    <Header />
+    <div className="bg-indigo-600 w-full h-screen"></div>
+    </>
+  )
+}
+
+export default App
